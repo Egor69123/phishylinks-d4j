943080ca05321e191a40b46ccd3dd8563c660f40
echertenko <echertenko@diasoft.ru>
2021-12-15 22:17:11 +0300

    Initial commit

--- src/main/java/me/jexom/phishylinks/config/SwaggerConfiguration.java
+++ src/main/java/me/jexom/phishylinks/config/SwaggerConfiguration.java
@@ -14,7 +14,7 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("me.jexom.phishylinks.main.java.me.jexom.phishylinks.controller"))
+                .apis(RequestHandlerSelectors.basePackage("me.jexom.phishylinks.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
